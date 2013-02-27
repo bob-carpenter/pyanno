aec59052fc18b74beb93502cf1111ea8a7355682
Becky Passonneau <becky@ccls.columbia.edu>
2013-02-27 18:38:14 -0500

    changes to mle_file.py so it runs now

--- examples/mle_file.py
+++ examples/mle_file.py
@@ -1,6 +1,8 @@
+import sys
 try:
     import pyanno.multinom
     import pyanno.util
+    import pyanno.kappa
 except ImportError, e:
     print e
     print ""
@@ -8,8 +10,8 @@ except ImportError, e:
     print "See instructions in Install.txt from pyanno distribution"
     raise SystemExit(1)
 
-if len(sys.argv) != 2:
-    print "Require name of data file as command-line argument."
+#if len(sys.argv) != 2:
+#    print "Require name of data file as command-line argument."
 
 # return (number-of-symbols, id-list, id-to-item-list, item-to-id-dict)
 def to_sym_tab(xsyms):
@@ -20,7 +22,7 @@ def to_sym_tab(xsyms):
     for xsym in xsyms:
         if xsym not in sym2id:
             sym2id[xsym] = next_id
-            id2sym.append(x)
+            id2sym.append(xsym)
             next_id += 1
         xs.append(sym2id[xsym])
     return (next_id,xs,id2sym,sym2id)
@@ -32,7 +34,7 @@ item_sym = []
 coder_sym = []
 label_sym = []
 file = open(file_name)
-int line_num = 1
+line_num = 1
 while True:
     line = file.readline()
     if not line:
@@ -47,8 +49,8 @@ while True:
     label_sym.append(fields[2])
     line_num += 1
 file.close()
-N = len(item)
-(I,item,item,item_id2sym,item_sym2id) = to_sym_tab(item_sym)
+N = len(item_sym)
+(I,item,item_id2sym,item_sym2id) = to_sym_tab(item_sym)
 (J,coder,coder_id2sym,coder_sym2id) = to_sym_tab(coder_sym)
 (K,label,label_id2sym,label_sym2id) = to_sym_tab(label_sym)
 
@@ -57,27 +59,33 @@ print "    prev=",pyanno.kappa.global_prevalence(item,label)
 
 print "RUNNING EM"
 (diff,ll,prev_mle,cat_mle,accuracy_mle) \
-    = pyanno.multinom.mle(item,anno,label,init_acc=0.6,epsilon=0.000001)
+    = pyanno.multinom.mle(item,coder,label,init_acc=0.6,epsilon=0.000001)
 
 print "CONVERGENCE ll[final] - ll[final-10]=",diff
 
 print "PREVALENCE ESTIMATES"
 print "{0:>2s}, {1:>5s}".format("k","MLE")
-for k in Ks:
+for k in range(K):
     print "{0:2d}, {1:5.3f}".format(k,prev_mle[k])
 
 print "ACCURACY ESTIMATES"
 print "{0:>3s},{1:>2s},{2:>2s}, {3:>5s}".format("j","k1","k2","mle")
-for j in Js:
-    for k1 in Ks:
-        for k2 in Ks:
+for j in range(J):
+    for k1 in range(K):
+        for k2 in range(K):
             print "{0:3d},{1:2d},{2:2d}, {3:5.3f}".\
                 format(j,k1,k2,accuracy_mle[j][k1][k2])
 
 print "CATEGORY ESTIMATES"
-for i in Is:
+for i in range(I):
     print "{0:5d}".format(i),
-    for k in Ks:
+    for k in range(K):
         print " {0:2d},{1:5.3f}".format(k,cat_mle[i][k]),
+#        print " {0:8s},{1:5.3f}".format(item_id2sym[k],cat_mle[i][k]),
     print ""
 
+print "ITEMS"
+abc = 0
+for xyz in item_id2sym:
+    print abc, xyz
+    abc+=1
